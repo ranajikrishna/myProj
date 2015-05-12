ecbe0fbeceee4fb09ba17f03cc7f32fa6d37eb9e
ranajikrishna <ranajikrishna@gmail.com>
2015-05-12 18:33:46 +0100

    final question11&12

--- myCodePy/ML/homeWork/final/question11.py
+++ myCodePy/ML/homeWork/final/question11.py
@@ -1,6 +1,5 @@
-
 #
-# Name: Final, Question 11.
+# Name: Final, Question 11 - 12
 #
 # Author: Ranaji Krishna.
 #
@@ -11,10 +10,22 @@
 # x_7 = (-2,0), y_7 = +1
 # Transform this training set into another two-dimensional space Z
 # z_1 =x_2 -2x_1-1, z_2 =x^2_1-2x_2 + 1
+#
+# Question 11:
 # Using geometry (not quadratic programming), what values of w (without w0) and b 
 # specify the separating plane w^Tz + b = 0 that maximizes the margin in the Z space? 
 # The values of w1, w2, b are:
 # -1,1,-0.5; 1,-1,-0.5; 1,0,-0.5; 0,1,-0.5 
+#
+# Question 12:
+# Consider the same training set of the previous problem, but instead of explicitly transforming 
+# the input space X, apply the hard-margin SVM algorithm with the kernel
+#			K(x, x') = (1 + x^Tx')^2
+# (which corresponds to a second-order polynomial transformation). Set up the expression 
+# for L(\alpha1...\alpha7) and solve for the optimal \alpha1, ..., \alpha7 (numerically, using a quadratic 
+# programming package). The number of support vectors you get is in what range?#
+#
+
 
 import sys;
 import numpy as np;
@@ -38,6 +49,17 @@ from numpy.linalg import matrix_rank
 
 import svmutil as svm
 
+
+# Supprt Vector analysis.
+def supVector(data, C):
+
+	options = '-s 0 -t 1 -d 2 -r 1 -g 1 -c ' + str(C); # Set Options.
+
+	prob = svm.svm_problem(np.array(data[2]).tolist(), np.array(data.loc[:,0:1]).tolist());	# Set problem.
+
+	model = svm.svm_train(prob, options);		   # Call libsvm.
+	
+	return(0);
 
 # Compute the sum the Euclidean distances between the points and the plane.
 def distance (data, w):
@@ -76,6 +98,7 @@ def main (argv = None):
 	data_train =  pd.DataFrame(data_train);
 	data_train = transform(data_train);
 
+	# --- Question 11 ---
 	w = pd.DataFrame(np.array([0,1,-0.5], dtype = float));		# Weights.
 	tmp1 = classify(data_train.loc[:,3:4], w);			# Classifcation.
 	data_train[5]=tmp1[0]; data_train[6]=tmp1[1];			# Populate the matrix.
@@ -84,6 +107,10 @@ def main (argv = None):
 	# Verification (0:match, 1:mis match). 
 	data_train[7] = [0 if data_train.loc[x,2]==data_train.loc[x,6] else 1 for x in data_train.index];
 
+	# --- Question 12 --- 
+	C = 1e6;		  	      # Setting a hard boundary.
+	supVector(data_train.loc[:,0:2], C);  # Support vector machine evaluation. 
+	
 	print(sum(data_train[7]));
 	print dist;
 	return(0);
