4ae13db383123b817da52bf66cbf3210146fc065
ranajikrishna <ranaji@intercom.io>
2015-06-12 18:29:41 -0700

    adding csv data

--- myCodeInt/activationMetric/sqlCode/cleanUniverse_v3.sql
+++ myCodeInt/activationMetric/sqlCode/cleanUniverse_v3.sql
@@ -27,11 +27,24 @@ AND schemaname = 'public';
 */
 
 
+/*
+select 
+app.id as app_id,
+subs.app_id as sub_app_id
+from subscriptions as subs
+left join apps as app on app.customer_id = subs.customer_id
+*/  
+
+
+
+
+
 
 select 
 t1.app_id,
 t1.first_payment,
 t1.second_payment,
+t2.tot_users,
 t2.avg_daily_users,
 t2.avg_daily_admins,
 t2.avg_daily_msgs,
@@ -39,6 +52,7 @@ t2.avg_daily_cmts,
 t2.avg_daily_mails,
 t2.avg_daily_cnvs,
 t3.sum_tot_dur,
+t2.old_id,
 t4.time_to_trial
 from (
 
@@ -68,8 +82,10 @@ left join
 
 select distinct
 das.app_id as app_id,
-osub.app_id as old_id,
-avg(das.users) as avg_daily_users,
+--osub.created_at as osub_ct,
+min(osub.id) as old_id,
+avg(das.daily_users) as avg_daily_users,
+max(das.users) as tot_users,
 round(avg(cast(daily_messages as double precision)),2) as avg_daily_msgs, 
 round(avg(cast(daily_comments as double precision)), 2) as avg_daily_cmts, 
 round(avg(cast(daily_conversations as double precision)),2) as avg_daily_cnvs, 
@@ -80,14 +96,16 @@ left join apps as aps on aps.id = das.app_id
 left join subscriptions as osub on osub.app_id = das.app_id
 left join billing_subscriptions as subs on subs.customer_id = aps.customer_id
 where to_char(das.created_at, '%Y%m%d') >= to_char(subs.started_at, '%Y%m%d') 
-and case when old_id <= 5675
+and case when osub.id <= 5675
 then 
 to_char(das.created_at, '%Y%m%d') <= to_char(subs.started_at + INTERVAL '30 days', '%Y%m%d')
 else 
 to_char(das.created_at, '%Y%m%d') <= to_char(subs.started_at + INTERVAL '14 days', '%Y%m%d') 
 end
-and subs.id is not null and aps.contains_any_intercomrades = 0
-group by das.app_id, osub.app_id 
+and subs.id is not null 
+and aps.contains_any_intercomrades = 0
+and aps.approved_to_send_email_status_id <> 3
+group by das.app_id--, osub.id, osub_ct 
 
 
 )  
@@ -102,12 +120,18 @@ pm.app_id as app_id,
 sum(abs(ss.duration_in_seconds)) as sum_tot_dur
 from "permissions" as pm
 left join apps as aps on aps.id = pm.app_id
+left join subscriptions as osub on osub.app_id = pm.app_id
 left join billing_subscriptions as subs on subs.customer_id = aps.customer_id
 left join admin_user_records as aur on aur.admin_id = pm.admin_id
 left join sessions as ss on ss.user_id = aur.user_id
 where subs.id is not null
 and ss.created_at > subs.started_at -- INTERVAL '1 days'
-and ss.created_at < subs.started_at + INTERVAL '14 days'
+and case when osub.id <= 5675
+then 
+ss.created_at < subs.started_at + INTERVAL '30 days'
+else
+ss.created_at < subs.started_at + INTERVAL '14 days'
+end
 and ss.app_id = 6
 group by pm.app_id
 
@@ -129,3 +153,7 @@ group by app_id, app_ct
 ) 
 
 as t4 on t4.app_id = t1.app_id
+
+
+
+--select * from daily_app_stats as das limit 10000
