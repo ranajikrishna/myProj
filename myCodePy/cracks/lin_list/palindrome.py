5e98e800facd4378cf0609d4d71c36953fa7f8a6
ranajikrishna <ranajikrishna@gmail.com>
2015-11-16 11:53:10 -0800

    palindrome corrected

--- myCodePy/cracks/lin_list/palindrome.py
+++ myCodePy/cracks/lin_list/palindrome.py
@@ -17,22 +17,20 @@ def is_palindrome(chk_list, node_bck, k):
 
 	if node_bck:
 		k += 1
-		node_for, m = is_palindrome(chk_list, node_bck.get_next(), k)
+		node_for, m, chk = is_palindrome(chk_list, node_bck.get_next(), k)
+		if chk == 0:
+			node_for = node_for.get_next()
+			return(node_for, floor(k/2), 0)
 	else:
 		node_for = chk_list.head()
-		return(node_for, floor(k/2))
+		return(node_for, floor(k/2), 1)
 
 	if (node_for.get_data() == node_bck.get_data()):
 		node_for = node_for.get_next()
-		if (k==1):
-			return(True)
-		else:
-			return(node_for, m)
+		return(node_for, m, 1)
 	else:
-		if (k==1):
-			return(False)
-		else:
-			return(node_for, m)
+		node_for = node_for.get_next()
+		return(node_for, m, 0)
 
 def main(argv = None):
 
@@ -40,10 +38,16 @@ def main(argv = None):
 	myList.add(1)
 	myList.add(2)
 	myList.add(3)
+	myList.add(1)
+	myList.add(3)
 	myList.add(2)
 	myList.add(1)
 		
-	print(is_palindrome(myList, myList.head(), 0))
+	node, mid, is_pal= is_palindrome(myList, myList.head(), 0)
+	if (is_pal == 0):
+		print("False")
+	else:
+	 	print("True")
 
 	return(0)
 
