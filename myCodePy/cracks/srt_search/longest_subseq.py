16af22bb619d7d9a4e700183165f9000fc0046dd
ranajikrishna <ranajikrishna@gmail.com>
2015-12-01 09:38:20 -0800

    Update longest_subseq.py

--- myCodePy/cracks/srt_search/longest_subseq.py
+++ myCodePy/cracks/srt_search/longest_subseq.py
@@ -5,7 +5,10 @@ Name: Longest subsequence.
 Author: Ranaji Krishna.
 
 Notes:
-A circus is designing a tower routine consisting of people standing atop one anoth- er's shoulders. For practical and aesthetic reasons, each person must be both shorter and lighter than the person below him or her. Given the heights and weights of each person in the circus, write a method to compute the largest possible number of people in such a tower.
+A circus is designing a tower routine consisting of people standing atop one anoth- er's shoulders. 
+For practical and aesthetic reasons, each person must be both shorter and lighter than the person below 
+him or her. Given the heights and weights of each person in the circus, write a method to compute the 
+largest possible number of people in such a tower.
 '''
 
 
