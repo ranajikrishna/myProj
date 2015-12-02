a7ecb80b5dd41fd7261801affef957972493b0ad
ranajikrishna <ranajikrishna@gmail.com>
2015-12-01 18:51:55 -0800

    search and rank1

--- myCodePy/cracks/srt_search/search_rank.py
+++ myCodePy/cracks/srt_search/search_rank.py
@@ -5,7 +5,10 @@ Name: Search and rank.
 Author: Ranaji Krishna.
 
 Notes:
-Imagine you are reading in a stream of integers. Periodically, you wish to be able to look up the rank of a number x (the number of values less than or equal to x). Imple- ment the data structures and algorithms to support these operations. That is,imple- mentthemethodtrack(int x), which is called when each number is generated, and the method get Rank Of'Number (int x), which returns thenumberof values less than or equal to x (not including x itself).
+Imagine you are reading in a stream of integers. Periodically, you wish to be able to look up the rank of a number 
+x (the number of values less than or equal to x). Imple- ment the data structures and algorithms to support these
+operations. That is,imple- mentthemethodtrack(int x), which is called when each number is generated, and the method 
+get Rank Of'Number (int x), which returns thenumberof values less than or equal to x (not including x itself).
 '''
 
 
